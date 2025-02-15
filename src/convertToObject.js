7a026dc120959fc48ba6cbde609f64a2e895a18f
Nataliia Parkhomchuk <n.polishchuk83@gmail.com>
2025-02-15 17:54:29 +0200

    Fix convertToObject implementation and lint errors

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,16 +5,18 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   const stylesArray = sourceString
-    .split(';') // Поділяємо рядок на окремі CSS-властивості
-    .map(item => item.trim()) // Видаляємо зайві пробіли
-    .filter(item => item !== ''); // Видаляємо порожні елементи
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item !== '');
 
   const stylesObject = {};
 
-  stylesArray.forEach(style => {
+  stylesArray.forEach((style) => {
     const [key, value] = style.split(':');
+
     if (key && value) {
       stylesObject[key.trim()] = value.trim();
     }
